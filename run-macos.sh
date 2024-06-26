36a92e691b0b19b9d2d83302e97c1614c8814ff7
B Horn <b@horn.uk>
2024-06-26 05:23:22 +0100

    port 80 to save space
    Signed-off-by: B Horn <b@horn.uk>

--- run-macos.sh
+++ run-macos.sh
@@ -1,2 +1,2 @@
 rm .m3u
-/Applications/VLC.app/Contents/MacOS/VLC -I macosx --extraintf http --http-password 5 ./payload.m3u8
+/Applications/VLC.app/Contents/MacOS/VLC -I macosx --extraintf http --http-password 5 --http-port 80 ./payload-port80.m3u8
